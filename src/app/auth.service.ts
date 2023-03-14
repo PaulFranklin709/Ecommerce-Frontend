7fb8ffff90876ce0d158b60cbed647eb28a2fc16
Paul Franklin <paul219@revature.net>
2023-03-14 16:35:55 -0400

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  http: HttpClient;
+  router: Router;
+  auth: any;
+  hasAuth: boolean;
 
-  constructor() { }
+  constructor(http: HttpClient, router: Router) {
+    this.http = http;
+    this.router = router;
+    this.auth = null;
+    this.hasAuth = false;
+  }
+
+  register(username: string, password: string) {
+    let url = "http://localhost:8080/users";
+
+    this.http.post(url, {
+      username: username, 
+      password: password
+    }).subscribe(
+      (response: any) => {
+        this.auth = response;
+        this.hasAuth = true;
+        // https://www.digitalocean.com/community/tutorials/angular-navigation-routerlink-navigate-navigatebyurl
+        this.router.navigateByUrl("/");
+      },
+      (error) => {console.log(error)}
+      );
+  }
+
+  isAuth(): boolean {
+    return this.hasAuth;
+  }
 }
