81d9833bd172818ab0bcdced02fd49c841dc72f2
Paul Franklin <paul219@revature.net>
2023-03-14 15:32:10 -0400

    Update signup.component.ts

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
@@ -7,10 +8,27 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  http: HttpClient;
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
   onSubmit(form: NgForm) {
     let username = form.value.username;
     let password = form.value.password;
     console.log(username);
     console.log(password);
+
+    let url = "http://localhost:8080/users";
+
+    this.http.post(url, {
+      username: username, 
+      password: password
+    }).subscribe(
+      (response: any) => {
+        console.log(response);
+      }
+    );
   }
 }
