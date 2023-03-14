4f461c43c6b0ad569ea13d5760cadef65afb4ac5
Paul Franklin <paul219@revature.net>
2023-03-14 16:36:09 -0400

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  authService: AuthService;
+  username: string;
 
+  constructor(authService: AuthService) {
+    this.authService = authService;
+    this.username = "";
+  }
+
+  showLogin(): boolean {
+    return !this.authService.isAuth();
+  }
+
+  showUsername(): boolean {
+    let showLogin = this.showLogin();
+
+    if (!showLogin) {
+      this.username = this.authService.auth.username;
+    }
+
+    return !showLogin;
+  }
 }
