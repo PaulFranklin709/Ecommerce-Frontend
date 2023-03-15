12b273750e40ab375f75ade2329fae846bc4e4fa
Paul Franklin <paul219@revature.net>
2023-03-15 10:38:33 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "signin", component: SigninComponent },
-  { path: "signup", component: SignupComponent }
+  { path: "signup", component: SignupComponent },
+  { path: "cart/:productId", component: CartComponent }
 ];
 
 @NgModule({
