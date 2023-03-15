06703e572d4c48037f4a2f76a3dfb48019773b97
Paul Franklin <paul219@revature.net>
2023-03-15 10:52:55 -0400

    Update cart.component.ts

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
+import { Product } from '../model/product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  products: Product[]
 
+  constructor(activatedRoute: ActivatedRoute, productService: ProductService, cartService: CartService) {
+    this.products = [];
+    this.products = cartService.products;
+
+    let productId = activatedRoute.snapshot.params["productId"];
+    let product = productService.getProductById(productId);
+
+    cartService.addToCart(product);
+    this.products = cartService.products;
+  }
 }
