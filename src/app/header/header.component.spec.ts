28bc96e27dc4de3aadf788d6861fe625235408ec
Paul Franklin <paul219@revature.net>
2023-03-22 15:28:06 -0400

    Update header.component.spec.ts

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -22,4 +22,24 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have as showLogin true', () => {
+    expect(component.showLogin()).toBeTrue();
+  });
+
+  it('should have as showLogin false', () => {
+    window.sessionStorage.setItem("auth", JSON.stringify({username: "testUser", password: "testPassword"}));
+    expect(component.showLogin()).toBeFalse();
+    window.sessionStorage.removeItem("auth");
+  });
+
+  it('should have as showUsername false', () => {
+    expect(component.showUsername()).toBeFalse();
+  });
+
+  it('should have as showUsername true', () => {
+    window.sessionStorage.setItem("auth", JSON.stringify({username: "testUser", password: "testPassword"}));
+    expect(component.showUsername()).toBeTrue();
+    window.sessionStorage.removeItem("auth");
+  });
 });
