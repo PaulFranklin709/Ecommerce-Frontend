6f6efeb05cef1438571d73e560cd80b3dcfa3359
Paul Franklin <paul219@revature.net>
2023-03-22 15:42:04 -0400

    Update product.service.spec.ts

--- src/app/product.service.spec.ts
+++ src/app/product.service.spec.ts
@@ -5,6 +5,19 @@ import { ProductService } from './product.service';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let response: Object[] = [];
+  response.push({
+    productId: "First Id",
+    productName: "First Product",
+    productDescription: "First Product Description",
+    productPrice: 1.00
+  });
+  response.push({
+    productId: "Second Id",
+    productName: "Second Product",
+    productDescription: "Second Product Description",
+    productPrice: 1.01
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,5 +28,34 @@ describe('ProductService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have as getProductsResponse products.length 2', () => {
+    service.getProductsResponse(response);
+    expect(service.products.length).toBe(2);
+  });
+
+  it('should have as getProductsResponse products.length 0', () => {
+    expect(service.products.length).toBe(0);
+  });
+
+  it('should have as getProductById productId "First Id"', () => {
+    service.getProductsResponse(response);
+    expect(service.getProductById("First Id").productId).toBe("First Id");
+  });
+
+  it('should have as getProductById productName "First Product"', () => {
+    service.getProductsResponse(response);
+    expect(service.getProductById("First Id").productName).toBe("First Product");
+  });
+
+  it('should have as getProductById productDescription "First Product Description"', () => {
+    service.getProductsResponse(response);
+    expect(service.getProductById("First Id").productDescription).toBe("First Product Description");
+  });
+
+  it('should have as getProductById productPrice 1.00', () => {
+    service.getProductsResponse(response);
+    expect(service.getProductById("First Id").productPrice).toBe(1.00);
   });
 });
