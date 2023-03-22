6ff9f7576f7a718e6565ceab101bd356dd3a515c
Paul Franklin <paul219@revature.net>
2023-03-22 15:21:00 -0400

    Update auth.service.spec.ts

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -16,4 +16,23 @@ describe('AuthService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have as auth null', () => {
+    expect(service.auth).toBe(null);
+  });
+
+  it('should have as hasAuth false', () => {
+    expect(service.hasAuth).toBeFalse();
+  });
+
+  it('should have as isAuth true', () => {
+    window.sessionStorage.setItem("auth", JSON.stringify({auth: true}));
+    expect(service.isAuth()).toBeTrue();
+    window.sessionStorage.removeItem("auth");
+  });
+
+  it('should have as isAuth false', () => {
+    expect(service.isAuth()).toBeFalse();
+  });
+
 });
